3dbf37ad765e12119f0749c453b16495299626de
Mariano Benitez <benitezmarianito@gmail.com>
2024-06-01 14:47:03 -0300

    x

---
+++ config/plugins.ts
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            ACL: "public-read",
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 60 * 60 * 24 * 7),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
---
+++ types/generated/components.d.ts
@@ -0,0 +1,28 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ComponentsMetadata extends Schema.Component {
+  collectionName: 'components_components_metadata';
+  info: {
+    displayName: 'Metadata';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.Text & Attribute.Required;
+    authors: Attribute.Relation<
+      'components.metadata',
+      'oneToMany',
+      'api::author.author'
+    >;
+    category: Attribute.Enumeration<['technology', 'design', 'business']> &
+      Attribute.Required;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'components.metadata': ComponentsMetadata;
+    }
+  }
+}

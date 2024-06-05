c11e277926a5b9328ecd87e59711a904781d1853
Mariano Benitez <benitezmarianito@gmail.com>
2024-06-05 10:13:52 -0300

    x

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,43 +1,53 @@
-export default ({ env }) => [
-  "strapi::logger",
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `${env("AWS_BUCKET")}` +
+const s3Config = (env) => ({
+  name: "strapi::security",
+  config: {
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "connect-src": ["'self'", "https:"],
+        "img-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "dl.airtable.com",
+          `${env("AWS_BUCKET")}` +
             ".s3." +
             `${env("AWS_REGION")}` +
             ".amazonaws.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            `${env("AWS_BUCKET")}` +
+        ],
+        "media-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "dl.airtable.com",
+          `${env("AWS_BUCKET")}` +
             ".s3." +
             `${env("AWS_REGION")}` +
             ".amazonaws.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
+        ],
+        upgradeInsecureRequests: null,
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+});
+
+export default ({ env }) => {
+  const allS3Variables =
+    env("AWS_REGION") &&
+    env("AWS_ACCESS_KEY_ID") &&
+    env("AWS_ACCESS_SECRET") &&
+    env("AWS_BUCKET");
+
+  return [
+    "strapi::logger",
+    "strapi::errors",
+    allS3Variables ? s3Config(env) : "strapi::security",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,26 +1,37 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_ACCESS_SECRET"),
-          },
-          region: env("AWS_REGION"),
-          params: {
-            ACL: "public-read",
-            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 60 * 60 * 24 * 7),
-            Bucket: env("AWS_BUCKET"),
-          },
+const s3Config = (env) => ({
+  name: "upload",
+  config: {
+    provider: "aws-s3",
+    providerOptions: {
+      s3Options: {
+        credentials: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+        },
+        region: env("AWS_REGION"),
+        params: {
+          ACL: "public-read",
+          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 60 * 60 * 24 * 7),
+          Bucket: env("AWS_BUCKET"),
         },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
+    },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
     },
   },
 });
+
+module.exports = ({ env }) => {
+  const allS3Variables =
+    env("AWS_REGION") &&
+    env("AWS_ACCESS_KEY_ID") &&
+    env("AWS_ACCESS_SECRET") &&
+    env("AWS_BUCKET");
+
+  return {
+    upload: allS3Variables ? s3Config(env) : {},
+  };
+};
